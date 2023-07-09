432b8252c9a328202e12271b43b4f4ca21a6fd2d
foxcreator <foxcreatorg@gmail.com>
2023-07-09 15:36:36 +0300

    [Prod 1.0.0]

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Маршруты корзины: Все товары в корзине, добавление, удаление одного, сохранение в БД, очистка корзины.
-Route::get('/cart', [App\Http\Controllers\CartController::class, 'index'])->name('cart');
-Route::post('/cart/add/{product}', [App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
-Route::post('/cart/remove/{product}', [App\Http\Controllers\CartController::class, 'remove'])->name('cart.remove');
-Route::post('/cart/checkout', [App\Http\Controllers\CartController::class, 'checkout'])->name('cart.checkout');
-Route::post('/cart/clear-cart', [App\Http\Controllers\CartController::class, 'clear'])->name('cart.clear');
+Route::get('/cart', [\App\Http\Controllers\CartController::class, 'index'])->name('cart');
+Route::post('/cart/add/{product}', [\App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
+Route::post('/cart/remove/{product}', [\App\Http\Controllers\CartController::class, 'remove'])->name('cart.remove');
+Route::post('/cart/checkout', [\App\Http\Controllers\CartController::class, 'checkout'])->name('cart.checkout');
+Route::post('/cart/clear-cart', [\App\Http\Controllers\CartController::class, 'clear'])->name('cart.clear');
 
 
 
